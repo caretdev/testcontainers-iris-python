2ae94ef15216ab30f4962419f7a9f03140bb1202
Dmitry Maslennikov <mrdaimor@gmail.com>
2023-08-22 15:46:51 +0400

    fixed setup

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ description = "InterSystems IRIS component of testcontainers-python."
 
 setup(
     name="testcontainers-iris",
-    version="1.0.1",
+    version="1.0.2",
     packages=find_namespace_packages(),
     description=description,
-    long_description=description,
+    long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     url="https://github.com/caretdev/testcontainers-iris-python",
     install_requires=[
