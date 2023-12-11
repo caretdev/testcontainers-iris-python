0069f41746c949139b75e33bc531a57db13f3e0a
Dmitry Maslennikov <mrdaimor@gmail.com>
2023-12-11 15:15:21 +0400

    updated setup files

--- setup.py
+++ setup.py
@@ -3,17 +3,7 @@ from setuptools import setup, find_namespace_packages
 description = "InterSystems IRIS component of testcontainers-python."
 
 setup(
-    name="testcontainers-iris",
-    version="1.1.0",
     packages=find_namespace_packages(),
     description=description,
     long_description=open('README.md').read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/caretdev/testcontainers-iris-python",
-    install_requires=[
-        "testcontainers-core",
-        "sqlalchemy",
-        "sqlalchemy-iris",
-    ],
-    python_requires=">=3.7",
 )
