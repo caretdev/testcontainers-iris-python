0894248d4937e1ceaaca010feb06847722bcf294
Dmitry Maslennikov <mrdaimor@gmail.com>
2025-05-03 20:51:06 +1000

    just checking

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_namespace_packages
 description = "InterSystems IRIS component of testcontainers-python."
 
 setup(
+    name="testcontainers-iris",
     packages=find_namespace_packages(),
     description=description,
     install_requires=[
